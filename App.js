d443ff844761e17408e8cd73b2bea2b15a5df7e4
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-07 16:03:29 -0800

    added text input field in view

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <TextInput>Name:</TextInput>
       <Text>This is my first mobile app</Text>
-      <Text>Fuck yeah!</Text>
+      <Text>YAY!</Text>
       <StatusBar style="auto" />
     </View>
   );
