7231ebc122c025e96bf1593f49f5935b724dd6cc
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-07 17:51:45 -0800

    added about file, need to add to app.js. Got the basics, start branch

---
+++ client/src/components/About.js
@@ -0,0 +1,36 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { Button, StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+
+function About({ }) {
+  return (
+    <View style={styles.container}>
+
+      <Text>About nnext</Text>
+      <Text>text why this game rocks and solves useful everyday needs</Text>
+
+      <Text>Introduction</Text>
+      <Text>Selecting inputs and outputs for your needs</Text>
+
+      <Text>How To Play</Text>
+      <Text>Step 1</Text>
+      <Text>Step 2</Text>
+      <Text>Step 3</Text>
+
+      <Text>Icons</Text>
+
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default About;
--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { Button, StyleSheet, Text, View, ScrollView, TextInput } from 'react-native';
+import About from './About';
 
 const App = () => {
+  const [openAbout, setOpenAbout] = useState(false);
+
   return (
     <View style={styles.container}>
       <TextInput>Name:</TextInput>
