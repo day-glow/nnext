eb43143fcccc0beccfa0c259682ca236dd589d48
Jacki Yanamura <jacki.yanamura+github@gmail.com>
2021-01-08 16:45:56 -0800

    color grid with 3 x 3 rows

--- client/src/components/GameAll.js
+++ client/src/components/GameAll.js
@@ -10,6 +10,26 @@ function GameAll({ show }) {
       <Text style={styles.outcomeType}>Party Mode: ALL</Text>
       <Text style={styles.numberOfPlayers}>show num of players</Text>
 
+      <View style={styles.row}>
+        <View style={styles.playerBox}/>
+        <View style={styles.playerBox}/>
+        <View style={styles.playerBox}/>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.playerBox}/>
+        <View style={styles.playerBox}/>
+        <View style={styles.playerBox}/>
+      </View>
+
+      <View style={styles.row}>
+        <View style={styles.playerBox}/>
+        <View style={styles.playerBox}/>
+        <View style={styles.playerBox}/>
+      </View>
+
+
+
       <Button
         style={styles.nnextBtn}
         onPress={() => {
@@ -25,13 +45,21 @@ function GameAll({ show }) {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   outcomeType: {
     fontSize: 50,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  playerBox: {
+    width: 100,
+    height: 100,
+    margin: 4,
+    backgroundColor: '#38E0B1',
   },
   players: {
     fontSize: 40,
